0705932976d5a226f54465c5c61a086677c5c68c
Annelies11 <mmahmudi1103@gmail.com>
2024-04-21 19:14:28 +0700

    buff chan

--- channel_test.go
+++ channel_test.go
@@ -69,3 +69,22 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(3 * time.Second)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+	go func() {
+
+		channel <- "Aris"
+		channel <- "Mahmudi"
+		channel <- "Mavis"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+}
